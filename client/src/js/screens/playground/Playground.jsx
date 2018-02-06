7ffffc3e9dd4db4a3d640bf68e4d3813c08c0d5a
joeonmars <joe@joeonmars.com>
2018-02-05 23:54:51 -0500

    update;

--- client/src/js/screens/playground/Playground.jsx
+++ client/src/js/screens/playground/Playground.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Chance from 'chance';
+import io from 'socket.io-client';
 
 import styles from './Playground.scss';
 
@@ -22,7 +23,7 @@ export default class Playground extends Component {
 
 	componentWillMount() {
 		const chance = new Chance();
-		
+
 		this.setState({
 			name: chance.name(),
 		});
@@ -30,6 +31,19 @@ export default class Playground extends Component {
 
 	componentDidMount() {
 
+		const socket = io('http://localhost:4200');
+
+		socket.on('connect', () => {
+			console.log('connect');
+
+			socket.once('server:connected', greeting => {
+				console.log('server:connected', greeting);
+			});
+		});
+
+		socket.on('disconnect', () => {
+			console.log('disconnect');
+		});
 	}
 
 	render() {
--- server/index.js
+++ server/index.js
@@ -2,8 +2,24 @@
 const express = require('express');  
 const app = express();  
 const server = require('http').createServer(app);  
+
 const io = require('socket.io')(server);
+const all_sockets = [];
+
+io.on('connect', socket => {  
+    console.log('Client connected!', socket.id);
+    all_sockets.push(socket);
+
+    socket.emit('server:connected', 'hello');
+
+    socket.on('disconnect', () => {
+    	console.log('Client disconnect!', socket.id);
+		const i = all_sockets.indexOf(socket);
+		all_sockets.splice(i, 1);
+	});
+});
+
 
 server.listen(4200);
 
-console.log("SERVER START!");
+console.log('SERVER START!');
