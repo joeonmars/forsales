502475f42bc56fe2824d576ae28c0ed9118a954e
joeonmars <joe@joeonmars.com>
2018-02-09 00:15:25 -0500

    added facebook signin

--- server/index.js
+++ server/index.js
@@ -1,10 +1,52 @@
 // app.js
-const express = require('express');  
+const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();  
-const server = require('http').createServer(app);  
+const server = require('http').createServer(app);
 
 const io = require('socket.io')(server);
 const nsp = io.of('/lobby');
+
+const passport = require('./passport');
+
+// Configure view engine to render
+// Create `ExpressHandlebars` instance with a default layout.
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Initialize Passport and restore authentication state,
+// if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/login/facebook',
+  passport.authenticate('facebook'),
+);
+
+app.get('/login/facebook/callback', (req, res, next) => {
+  passport.authenticate('facebook', 
+  	(err, user, info) => {
+  		if (err) {
+  			return next(err);
+  		}
+
+  		if (!user) {
+  			console.log(info);
+  		}
+
+    	res.json(user);
+  	})(req, res, next)
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 
 const Table = require('./models/Table.js');
 //const Chance = require('chance');
---
+++ server/passport.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+// Facebook App
+// https://developers.facebook.com/apps/177422786204498/settings/basic/
+
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new FacebookStrategy({
+    clientID: '177422786204498', //process.env.CLIENT_ID
+    clientSecret: '212f8c70ab460f524e47ccfb05a8c405', //process.env.CLIENT_SECRET
+    callbackURL: 'http://localhost:4200/login/facebook/callback',
+    profileFields: ['id', 'name', 'picture.type(large)', 'emails', 'displayName', 'gender'],
+    enableProof: true,
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  })
+);
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+
+module.exports = passport;
